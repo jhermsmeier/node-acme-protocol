8463f6e63d6a22338f245e2c4e6d1f0eb7945d74
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-07-04 14:54:12 +0200

    Update test,lib: Trim whitespace

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var assert = require( 'assert' )
 var nock = require( 'nock' )
 
 suite( 'ACME', function() {
-  
+
   var acme = new ACME({
     baseUrl: 'https://mock_encrypt.example.com',
   })
@@ -18,15 +18,15 @@ suite( 'ACME', function() {
       new ACME({ baseUrl: {} })
     })
   })
-  
+
   test( '#getDirectory()', function( next ) {
-    var payload = { 
+    var payload = {
       'new-authz': 'https://mock_encrypt.example.com/acme/new-authz',
       'new-cert': 'https://mock_encrypt.example.com/acme/new-cert',
       'new-reg': 'https://mock_encrypt.example.com/acme/new-reg',
-      'revoke-cert': 'https://mock_encrypt.example.com/acme/revoke-cert' 
+      'revoke-cert': 'https://mock_encrypt.example.com/acme/revoke-cert'
     }
-    
+
     nock( 'https://mock_encrypt.example.com' )
       .get( '/directory' )
       .reply( 200, payload )
@@ -38,5 +38,5 @@ suite( 'ACME', function() {
       next()
     })
   })
-  
+
 })
