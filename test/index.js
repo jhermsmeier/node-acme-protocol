7c48ddb0ba38a6ac2e9ee24d780e8ce779b6f639
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-07-04 15:18:34 +0200

    Update test: Simplify, compact formatting

--- test/index.js
+++ test/index.js
@@ -4,39 +4,35 @@ var nock = require( 'nock' )
 
 suite( 'ACME', function() {
 
-  var acme = new ACME({
-    baseUrl: 'https://mock_encrypt.example.com',
+  var client = new ACME({
+    baseUrl: 'https://api.example.com',
   })
 
   test( 'throws an error if baseUrl is not a string', function() {
-    assert.throws( function() {
-      new ACME({ baseUrl: 42 })
-    })
-
-
-    assert.throws( function() {
-      new ACME({ baseUrl: {} })
-    })
+    assert.throws( function() { new ACME({ baseUrl: 42 }) })
+    assert.throws( function() { new ACME({ baseUrl: {} }) })
   })
 
   test( '#getDirectory()', function( next ) {
+
     var payload = {
-      'new-authz': 'https://mock_encrypt.example.com/acme/new-authz',
-      'new-cert': 'https://mock_encrypt.example.com/acme/new-cert',
-      'new-reg': 'https://mock_encrypt.example.com/acme/new-reg',
-      'revoke-cert': 'https://mock_encrypt.example.com/acme/revoke-cert'
+      'new-authz': 'https://api.example.com/acme/new-authz',
+      'new-cert': 'https://api.example.com/acme/new-cert',
+      'new-reg': 'https://api.example.com/acme/new-reg',
+      'revoke-cert': 'https://api.example.com/acme/revoke-cert'
     }
 
-    nock( 'https://mock_encrypt.example.com' )
+    nock( 'https://api.example.com' )
       .get( '/directory' )
       .reply( 200, payload )
 
-    acme.getDirectory( function( error, data ) {
+    client.getDirectory( function( error, data ) {
       assert.ifError( error )
       assert.ok( data )
       assert.deepStrictEqual( payload, data )
       next()
     })
+
   })
 
 })
